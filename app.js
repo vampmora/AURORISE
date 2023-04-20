548d4fcf25264240cf50edacb8ab06eb5fb37217
Amr Elmansoury <74483757+vampmora@users.noreply.github.com>
2023-04-20 11:45:08 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,40 @@ let scene = document.getElementById('scene');
 let parallaxInstance = new Parallax(scene);
 
 
+$(document).ready(()=>{
+    $('.head .logo .webName').lettering();
+    let tl = gsap.timeline();
+    tl.from('.head .logo', {
+        x:-20,
+        stagger:.1,
+        duration:2,
+        opacity:0
+    })
+})
+
+
+$(document).ready(()=>{
+    $('.name span').lettering();
+    let tl = gsap.timeline();
+    tl.from('.name  span  ',  {
+        x:-20,
+        stagger: .1,
+        duration:1,
+        opacity:0
+    })
+})
+
+
+$(document).ready(()=>{
+    $('.navbar a .link  ').lettering();
+    let tl = gsap.timeline();
+    tl.from('.navbar   ',  {
+        x:50,
+        stagger: .1,
+        duration:2,
+        opacity:0
+    })
+})
 
 $(document).ready(()=>{
     $('.bottomLeft p ').lettering();
@@ -16,27 +50,14 @@ $(document).ready(()=>{
 
 
 $(document).ready(()=>{
-    $('.name  span').lettering();
+    $('.bottomRight a i').lettering();
+
     let tl = gsap.timeline();
-    tl.from('.name  span  ',  {
-        x:-20,
-        stagger: .1,
+    tl.from('.bottomRight a ', {
+        x:50,
+        stagger:.1,
         duration:1,
         opacity:0
     })
 })
-
-
-
-$(document).ready(()=>{
-    $('.navbar a .link  ').lettering();
-    let tl = gsap.timeline();
-    tl.from('.navbar   ',  {
-        x:50,
-        stagger: .1,
-        duration:2,
-        opacity:0
-    })
-})
-
  
